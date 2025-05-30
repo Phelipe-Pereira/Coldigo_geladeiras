f27daf304f519b7370bc1dddfe60df2a4c0a2021
Phelipe Leandro Pereira <phelipe.pereira0811@hotmail.com>
2025-05-29 21:39:38 -0300

    Subindo projeto pro github

---
+++ bin/src/main/webapp/index.jsp
@@ -0,0 +1,37 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Coldigo geladeiras</title>
+    <link rel="stylesheet" type="text/css" href="css/site.css" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+<header></header>
+<nav></nav>
+<section>
+    <div id="topo">
+        <img src="imgs/geladeira1.png" alt="Imagem de uma geladeira" class="geladeira esquerda"/>
+        <div id="titulos">
+            <h2>Bem-vindo ao site da Coldigo geladeiras!</h2>
+            <h3>A maior loja de geladeiras da região</h3>
+        </div>
+        <img src="imgs/geladeira2.png" alt="Imagem de uma geladeira" class="geladeira esquerda"/>
+    </div>
+    <p>
+        Coldigo geladeiras é a maior empresa de revenda de geladeiras,
+        refrigeradores, freezers e afins da região. Desde 2007 gelando e
+        congelando para seu prazer.
+    </p>
+    <p>
+        Sua geladeira estragou? Está precisando de uma maior? Quer mais conforto
+        e praticidade? Já pensou em fazer um consórcio de geladeira? Venha nos
+        fazer uma visita ou entre em contato para mais informações.
+    </p>
+</section>
+</body>
+    <footer></footer>
+<script src="js/site.js"></script>
+</html>
---
+++ out/artifacts/ProjetoTrilhaWebInte_war_exploded/js/site.js
@@ -0,0 +1,92 @@
+function valideFaleConosco(){
+    var nome = document.formFaleConosco.txtName.value;
+    var expRegNome = new RegExp("^[A-zÀ-ü]{3,}([]{1}[A-zÀ-ü]{2,}+$");
+    
+    if(!expRegNome.test(nome)){
+        alert("Preencha seu nome!");    
+        document.formFaleConosco.txtName.focus();
+        return false;
+    }
+
+    var fone = document.formFaleConosco.txtTelefone.value;
+    var expRegFone = new RegExp("^[(]{1}[1-9]{2}[)]{1}[0-9]{4,5}[-]{1}[0-9]{4}$");
+
+    if(!expRegFone.test(fone)){
+        alert("Preencha seu telefone!");    
+        document.formFaleConosco.txtTelefone.focus();
+        return false;
+    }
+
+    if(document.formFaleConosco.txtEmail.value==""){
+        alert("Preencha seu email!");    
+        document.formFaleConosco.txtEmail.focus();
+        return false;
+    }
+
+    if (document.formFaleConosco.selMotivo.value == "PR") {
+        var produtoSelect = document.formFaleConosco.selProduto;
+        if (produtoSelect && produtoSelect.value == "") {
+            alert("Selecione o produto desejado.");    
+            produtoSelect.focus();
+            return false;
+        }
+    }
+
+    if(document.formFaleConosco.comentario.value==""){
+        alert("Insira um comentário!");    
+        document.formFaleConosco.comentario.focus();
+        return false;
+    }
+
+
+    return true;
+}
+
+
+function verificarMotivo(motivo){
+    var elemento = document.getElementById("opcaoProduto");
+
+    if(motivo == "PR"){
+        
+        var select = document.createElement("select")
+        select.setAttribute("name", "selProduto")
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "");
+        
+        var texto = document.createTextNode("Escolha");
+
+        option.appendChild(texto)
+
+        select.appendChild(option)
+
+        var option = document.createElement("option");
+        option.setAttribute("value", "FR");
+
+        var texto = document.createTextNode("Freezer");
+        option.appendChild(texto);
+
+        select.appendChild(option);
+        
+        var option = document.createElement("option");
+        option.setAttribute("value", "GE");
+
+        var texto = document.createTextNode("Geladeira");
+
+        option.appendChild(texto);
+
+        select.appendChild(option);
+
+        elemento.appendChild(select);
+    }else{
+        if(elemento.firstChild){
+            elemento.removeChild(elemento.firstChild);
+        }
+    }
+}
+
+$(document).ready(function () {
+    $("header").load("/ProjetoTrilhaWebInte_war_exploded/pages/sites/general/cabecalho.html")
+    $("nav").load("/ProjetoTrilhaWebInte_war_exploded/pages/sites/general/menu.html")
+    $("footer").load("/ProjetoTrilhaWebInte_war_exploded/pages/sites/general/rodape.html")
+})
